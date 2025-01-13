38762bc010aede4103e0d9a18c02e13d81cc6537
TaeHee00 <kancth03@gmail.com>
2025-01-14 00:25:26 +0900

    first commit

---
+++ src/test/java/com/kancth/nomadbackend/NomadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kancth.nomadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
