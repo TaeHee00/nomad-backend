1383ed7dcd344f6572f58bc1b88b97e3ba40d5b1
Tae Hee Jang <kancth03@gmail.com>
2025-01-15 00:55:55 +0900

    feat/#4: User Dto -> Entity(toEntity) 메서드 구현

--- src/main/java/com/kancth/nomad/domain/user/entity/User.java
+++ src/main/java/com/kancth/nomad/domain/user/entity/User.java
@@ -1,8 +1,10 @@
 package com.kancth.nomad.domain.user.entity;
 
+import com.kancth.nomad.domain.user.dto.SignUpRequest;
 import com.kancth.nomad.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
@@ -11,6 +13,7 @@ import org.hibernate.annotations.SQLRestriction;
 @Table
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @SQLRestriction("deleted_at IS NULL")
 @SQLDelete(sql = "UPDATE user SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
 public class User extends BaseEntity {
@@ -28,4 +31,15 @@ public class User extends BaseEntity {
     private String profileImgUrl;
     // 이메일 인증 여부
     private boolean verified;
+
+    public static User toEntity(SignUpRequest request) {
+        return User.builder()
+                .email(request.email())
+                .name(request.name())
+                .nickname(request.nickname())
+                .loginId(request.loginId())
+                .password(request.password())
+                .verified(false)
+                .build();
+    }
 }
