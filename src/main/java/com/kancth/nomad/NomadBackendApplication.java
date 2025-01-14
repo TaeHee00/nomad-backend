1c7a897a833e23210992a323a5147151ad07210f
Tae Hee Jang <kancth03@gmail.com>
2025-01-14 22:22:11 +0900

    fix: package명 변경

--- src/main/java/com/kancth/nomad/NomadBackendApplication.java
+++ src/main/java/com/kancth/nomad/NomadBackendApplication.java
@@ -1,4 +1,4 @@
-package com.kancth.nomadbackend;
+package com.kancth.nomad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/kancth/nomad/NomadBackendApplicationTests.java
+++ src/test/java/com/kancth/nomad/NomadBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kancth.nomadbackend;
+package com.kancth.nomad;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
