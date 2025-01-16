5d8f0e5f547e78cab3954ef51b5610ed6ec4a3f6
Tae Hee Jang <kancth03@gmail.com>
2025-01-17 00:25:39 +0900

    feat/#4: User Controller SignUp Fail Test  구현

--- src/test/java/com/kancth/nomad/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/kancth/nomad/domain/user/controller/UserControllerTest.java
@@ -1,13 +1,11 @@
 package com.kancth.nomad.domain.user.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kancth.nomad.domain.user.dto.SignUpRequest;
 import com.kancth.nomad.domain.user.entity.User;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,8 +17,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
@@ -42,30 +39,52 @@ class UserControllerTest {
     void tearDown() {
     }
 
-    @DisplayName("SignUp API 성공")
-    @Test
-    void signUp() throws Exception {
-        SignUpRequest request = SignUpRequest.builder()
-                .loginId("testSignUpId")
-                .email("testSignUpEmail@nomad.com")
-                .name("김철수")
-                .nickname("테스터")
-                .password("testPw1!")
-                .build();
+    @Nested
+    class SignUp {
+        @DisplayName("SignUp API 성공")
+        @Test
+        void signUp() throws Exception {
+            SignUpRequest request = SignUpRequest.builder()
+                    .loginId("testSignUpId")
+                    .email("testSignUpEmail@nomad.com")
+                    .name("김철수")
+                    .nickname("테스터")
+                    .password("testPw1!")
+                    .build();
 
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/api/v1/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(request)))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andReturn();
-        User.Response response = mapper.readValue(mvcResult.getResponse().getContentAsString(), User.Response.class);
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/api/v1/users")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(mapper.writeValueAsString(request)))
+                    .andExpect(MockMvcResultMatchers.status().isCreated())
+                    .andReturn();
+            User.Response response = mapper.readValue(mvcResult.getResponse().getContentAsString(), User.Response.class);
 
-        Assertions.assertThat(response.id()).isNotNull();
-        Assertions.assertThat(request.loginId()).isEqualTo(response.loginId());
-        Assertions.assertThat(request.email()).isEqualTo(response.email());
-        Assertions.assertThat(request.name()).isEqualTo(response.name());
-        Assertions.assertThat(request.nickname()).isEqualTo(response.nickname());
-        Assertions.assertThat(request.password()).isEqualTo(response.password());
-        Assertions.assertThat(response.verified()).isFalse();
+            Assertions.assertThat(response.id()).isNotNull();
+            Assertions.assertThat(request.loginId()).isEqualTo(response.loginId());
+            Assertions.assertThat(request.email()).isEqualTo(response.email());
+            Assertions.assertThat(request.name()).isEqualTo(response.name());
+            Assertions.assertThat(request.nickname()).isEqualTo(response.nickname());
+            Assertions.assertThat(request.password()).isEqualTo(response.password());
+            Assertions.assertThat(response.verified()).isFalse();
+        }
+
+        @DisplayName("SignUp API 실패")
+        @Test()
+        void signUpFail() throws Exception {
+            SignUpRequest request = SignUpRequest.builder()
+                    .loginId("testSignUpId2")
+                    // NOTE: Email 형식 오류
+                    .email("testSignUpEmailnomad.com")
+                    .name("김칠수")
+                    .nickname("테스터1")
+                    .password("testPw1!")
+                    .build();
+
+            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/api/v1/users")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(mapper.writeValueAsString(request)))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andReturn();
+        }
     }
 }
