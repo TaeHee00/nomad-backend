b3b22299203d885ba7643a39a9da3c693f5309bd
Tae Hee Jang <kancth03@gmail.com>
2025-01-17 22:35:30 +0900

    feat/#4: SignUp 시 Password를 Encoding하여 저장하도록 변경

--- src/main/java/com/kancth/nomad/domain/user/service/UserService.java
+++ src/main/java/com/kancth/nomad/domain/user/service/UserService.java
@@ -5,11 +5,14 @@ import com.kancth.nomad.domain.user.entity.User;
 import com.kancth.nomad.domain.user.exception.EmailAlreadyExistsException;
 import com.kancth.nomad.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     // 회원가입 메서드
@@ -17,9 +20,10 @@ public class UserService {
         checkLoginIdDuplicate(request.loginId());
         checkEmailDuplicate(request.email());
 
-        // TODO: Password Encoding
-        // TODO: Test Code 작성
-        return userRepository.save(User.toEntity(request));
+        // Password Encoding
+        String encodedPassword = passwordEncoder.encode(request.password());
+
+        return userRepository.save(User.toEntity(request, encodedPassword));
     }
 
     private void checkEmailDuplicate(String email) {
