66bc8de1a47e0faa53a2a9801ff6cb57e1c586ca
Tae Hee Jang <kancth03@gmail.com>
2025-01-18 22:54:07 +0900

    feat: ProjectWorkScope Entity 구현

---
+++ src/main/java/com/kancth/nomad/domain/project/entity/ProjectWorkScope.java
@@ -0,0 +1,30 @@
+package com.kancth.nomad.domain.project.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@SQLRestriction("deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE project_work_scope SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+public class ProjectWorkScope {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "work_scope_id")
+    private WorkScope workScope;
+}
