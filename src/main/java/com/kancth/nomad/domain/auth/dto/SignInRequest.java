322c774b2395dd0be2022bd635db7d68f0e0db13
Tae Hee Jang <kancth03@gmail.com>
2025-01-18 14:50:22 +0900

    feat: 테스트를 위한 SignInRequest Builder 등록

--- src/main/java/com/kancth/nomad/domain/auth/dto/SignInRequest.java
+++ src/main/java/com/kancth/nomad/domain/auth/dto/SignInRequest.java
@@ -1,7 +1,9 @@
 package com.kancth.nomad.domain.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record SignInRequest(
         @NotBlank(message = "아이디가 입력되지 않았습니다.")
         String loginId,
