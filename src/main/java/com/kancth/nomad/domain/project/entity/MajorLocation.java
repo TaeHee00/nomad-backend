90907aa0931b55e14dc5b0884fafd8ee185614b0
Tae Hee Jang <kancth03@gmail.com>
2025-01-18 22:51:01 +0900

    feat: MajorLocation Entity 구현

---
+++ src/main/java/com/kancth/nomad/domain/project/entity/MajorLocation.java
@@ -0,0 +1,24 @@
+package com.kancth.nomad.domain.project.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@SQLRestriction("deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE major_location SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+public class MajorLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
