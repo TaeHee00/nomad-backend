f4536bc2677032f3579ba72bd4031f1073979ef6
Tae Hee Jang <kancth03@gmail.com>
2025-01-18 13:32:18 +0900

    feat: SignIn Request Body Class 구현

---
+++ src/main/java/com/kancth/nomad/domain/auth/dto/SignInRequest.java
@@ -0,0 +1,11 @@
+package com.kancth.nomad.domain.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record SignInRequest(
+        @NotBlank(message = "아이디가 입력되지 않았습니다.")
+        String loginId,
+        @NotBlank(message = "비밀번호가 입력되지 않았습니다.")
+        String password
+) {
+}
